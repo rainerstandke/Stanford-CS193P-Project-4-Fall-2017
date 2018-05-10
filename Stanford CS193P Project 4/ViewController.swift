04421bfda188c6b58b59378a86da1042e81adae6
Rainer Standke <rainer@xmil.biz>
2018-05-10 12:40:40 -0700

    Initial Commit

---
+++ Stanford CS193P Project 4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stanford CS193P Project 4
+//
+//  Created by Rainer Standke on 5/10/18.
+//  Copyright © 2018 Rainer Standke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
